52af259cac474e10f6f1ed095ad053b4da117e6e
Ethan Winn <ethan@colab.coop>
2022-09-21 21:57:34 -0400

    Add params for compressor

--- lib/kickbox_engine.lua
+++ lib/kickbox_engine.lua
@@ -2,10 +2,10 @@ local Kickbox = {}
 local Formatters = require 'formatters'
 
 local specs = {
-	["freq"] = controlspec.new(20, 1000, "exp", 0, 36, "Hz"),
-	["mod_ratio"] = controlspec.new(0, 10, "lin", 0, 1, ""),
+	["freq"] = controlspec.new(20, 1000, "exp", 0, 44, "Hz"),
+	["mod_ratio"] = controlspec.new(0, 10, "lin", 0, 2, ""),
 	["amp"] = controlspec.new(0, 1, "lin", 0, 1, ""),
-	["sustain"] = controlspec.new(0, 1, "lin", 0, 0.5, ""),
+	["sustain"] = controlspec.new(0, 1, "lin", 0, 0.33, ""),
 	["contour"] = controlspec.new(-5, 5, "lin", 0, -3, ""),
 	["click_length"] = controlspec.new(0, 1, "lin", 0, 0.2, ""),
 	["click_curve"] = controlspec.new(-5, 5, "lin", 0, -4, ""),
@@ -20,7 +20,7 @@ local specs = {
 	["mod_index"] = controlspec.new(0, 10, "lin", 0, 0, ""),
 	["mod_feedback"] = controlspec.new(0, 1, "lin", 0, 0, ""),
 	["mod_sweep_amt"] = controlspec.new(0, 10, "lin", 0, 0, ""),
-	["sin_shaper_amt"] = controlspec.new(-1, 4, "lin", 0, 1, "") 
+	["sin_shaper_amt"] = controlspec.new(0, 4, "lin", 0, 1, "") 
 }
 
 local param_names = {
@@ -45,9 +45,45 @@ local param_names = {
   "sin_shaper_amt"
 }
 
-function Kickbox.add_params()
-  params:add_group("Kickbox Engine",#param_names)
+local glue_specs = {
+	["drum_comp_mix"] = controlspec.new(0, 1, "lin", 0, 1, ""),
+	["in_sidechain_mix"] = controlspec.new(0, 1, "lin", 0, 1, ""),
+	["in_sig_lvl"] = controlspec.new(0, 2, "lin", 0, 1, ""),
+	["drum_sig_lvl"] = controlspec.new(0, 2, "lin", 0, 0.5, ""),
+	["drum_cntrl_lvl"] = controlspec.new(0, 2, "lin", 0, 1, ""),
+	["sidechain_makeup_amt"] = controlspec.new(0, 1, "lin", 0, 0, ""),
+	["sidechain_ratio"] = controlspec.new(0, 8, "lin", 0, 0.5, ""),
+	["sidechain_thresh"] = controlspec.new(0, 1, "lin", 0, 0.3, ""),
+	["sidechain_release"] = controlspec.new(0, 1, "lin", 0, 0.5, ""),
+	["sidechain_attack"] = controlspec.new(0, 1, "lin", 0, 0.05, ""),
+	["comp_makeup_amt"] = controlspec.new(0, 1, "lin", 0, 1, ""),
+	["comp_ratio"] = controlspec.new(0, 8, "lin", 0, 2, ""),
+	["comp_thresh"] = controlspec.new(0, 1, "lin", 0, 0.5, ""),
+	["comp_release"] = controlspec.new(0, 1, "lin", 0, 0.1, ""),
+	["comp_attack"] = controlspec.new(0, 1, "lin", 0, 0.05, "")
+}
 
+local glue_param_names = {
+	"drum_comp_mix",
+	"in_sidechain_mix",
+	"in_sig_lvl",
+	"drum_sig_lvl",
+	"drum_cntrl_lvl",
+	"sidechain_makeup_amt",
+	"sidechain_ratio",
+	"sidechain_thresh",
+	"sidechain_release",
+	"sidechain_attack",
+	"comp_makeup_amt",
+	"comp_ratio",
+	"comp_thresh",
+	"comp_release",
+	"comp_attack"
+}
+
+function Kickbox.add_params()
+  params:add_group("Kickbox Engine", #param_names + #glue_param_names + 2)
+  params:add_separator("Drum")
   for i = 1,#param_names do
     local p_name = param_names[i]
     params:add{
@@ -55,14 +91,24 @@ function Kickbox.add_params()
       id = "kickbox_engine_"..p_name,
       name = p_name,
       controlspec = specs[p_name],
-      -- formatter = p_name == "pan" and Formatters.bipolar_as_pan_widget or nil,
       action = function(x)
-        print(p_name) 
         engine[p_name](x) 
       end
     }
   end
-  
+  params:add_separator("Compressor")
+  for i = 1,#glue_param_names do
+    local p_name = glue_param_names[i]
+    params:add{
+      type = "control",
+      id = "kickbox_engine_"..p_name,
+      name = p_name,
+      controlspec = glue_specs[p_name],
+      action = function(x)
+        engine[p_name](x) 
+      end
+    }
+  end
   params:bang()
 end
 
